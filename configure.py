30d882c3d617ad94426a89bfe4bb6811c2e82fce
Lucas Alves <lucas.developing@gmail>
2020-09-16 09:08:37 -0300

    Agora é possível obter o acess_token realmente.

--- configure.py
+++ configure.py
@@ -1,6 +1,7 @@
 import src.console as console
 import src.configurations.for_twitter as twitter_config
 import os
+import sys
 
 """
 Script de configuração.
@@ -9,9 +10,13 @@ os.environ['DOT_ENV_PATH'] = os.getcwd() + '/.env'
 
 console.printLine("Configurar")
 if console.getInputAsBoolean('Deseja mesmo configurar? Qualquer configuração anterior será desfeita...') is True:
-    if os.path.isfile(os.getenv('DOT_ENV_PATH')) is True:
-        os.remove('.env')
-    configurationFile = open(os.environ['DOT_ENV_PATH'], 'a')
-    if console.getInputAsBoolean('Gostaria de configurar o Twitter?') is True:
-        console.printLine('-- Twitter --')
-        twitter_config.configureTwitter(configurationFile)
+    try:
+        if os.path.isfile(os.getenv('DOT_ENV_PATH')) is True:
+            os.remove('.env')
+        configurationFile = open(os.environ['DOT_ENV_PATH'], 'a')
+        if console.getInputAsBoolean('Gostaria de configurar o Twitter?') is True:
+            console.printLine('-- Twitter --')
+            twitter_config.configureTwitter(configurationFile)
+    except OSError:
+        console.printErrorLine("Por favor dê permissão para criação de arquivos na pasta, dica: chmod -R 777 *.")
+        sys.exit()
